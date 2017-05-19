828a4475db49faf7bafed346894485bcda45a891
Joshua Peek <josh@joshpeek.com>
2017-05-19 13:38:53 -0700

    Skip token prompt if file already exists

--- scripts/getToken.js
+++ scripts/getToken.js
@@ -3,11 +3,18 @@ process.stdin.setEncoding("utf8")
 var util = require("util")
 var fs = require("fs")
 
+var filename = "src/token.js"
+
+if (fs.existsSync(filename)) {
+  process.exit()
+  return
+}
+
 console.log("Enter your GitHub API token:")
 process.stdin.on("data", function(text) {
   var tokenJS = `module.exports = "${text.trim()}"\n`
 
-  fs.writeFile("src/token.js", tokenJS, function(err) {
+  fs.writeFile(filename, tokenJS, function(err) {
     console.log("API token saved.")
     process.exit()
   })
